f23b9b1698f4a29f20125289dde35d2707c6b8df
项目维护者 <project-maintainer@local.invalid>
2026-09-12 22:17:33 +0800

    明确后端依赖与开发约定

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,3 @@
-plugins { kotlin("jvm") version "2.0.0" }
+plugins { kotlin("jvm") version "2.1.0"; application }
+repositories { mavenCentral() }
+dependencies { implementation("io.ktor:ktor-server-core-jvm:3.0.3"); implementation("io.ktor:ktor-server-netty-jvm:3.0.3"); implementation("com.mysql:mysql-connector-j:9.2.0"); implementation("org.apache.kafka:kafka-clients:3.9.0"); testImplementation(kotlin("test")) }
